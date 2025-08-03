9c0e29e989f68d64664c060e3b1d284abdebc9c5
afornerot <afornerot@cadoles.com>
2025-08-03 22:42:31 +0200

    release v1.1.4

--- Resources/public/bninemdeditor.js
+++ Resources/public/bninemdeditor.js
@@ -1,20 +1,23 @@
 // bninemdeditor.js
 
-// Dynamically inject EasyMDE CSS
+// Inject CSS
 const cssLink = document.createElement('link');
 cssLink.rel = 'stylesheet';
 cssLink.href = '/bundles/bninemdeditor/easymde.min.css';
 document.head.appendChild(cssLink);
 
-// Dynamically inject EasyMDE JS
+// Inject JS
 const script = document.createElement('script');
 script.src = '/bundles/bninemdeditor/easymde.min.js';
 script.onload = function () {
     document.querySelectorAll('.easymde-textarea').forEach(function (textarea, index) {
+        const height = textarea.dataset.markdownHeight;
+
         new EasyMDE({
             element: textarea,
             spellChecker: false,
             status: false,
+            minHeight: height ? `${height}px` : undefined,
             autosave: {
                 enabled: false,
                 uniqueId: "markdownEditor_" + index,
--- src/Form/Type/MarkdownType.php
+++ src/Form/Type/MarkdownType.php
@@ -4,6 +4,8 @@ namespace Bnine\MdEditorBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MarkdownType extends AbstractType
@@ -22,7 +24,17 @@ class MarkdownType extends AbstractType
             ],
             'required' => false,
             'label' => 'Editeur Markdown',
+            'markdown_height' => null,
         ]);
+
+        $resolver->setAllowedTypes('markdown_height', ['null', 'int']);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        if ($options['markdown_height']) {
+            $view->vars['attr']['data-markdown-height'] = $options['markdown_height'];
+        }
     }
 
     public function getBlockPrefix(): string
